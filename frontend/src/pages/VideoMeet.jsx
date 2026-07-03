843a5fdb4055922494020f0c2f208c8f9c88707b
priyankashori <shorishoripriyanka@gmail.com>
2026-07-03 12:26:50 +0530

    Change server_url assignment to use server directly

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -13,7 +13,7 @@ import StopScreenShareIcon from "@mui/icons-material/StopScreenShare";
 import ChatIcon from "@mui/icons-material/Chat";
 import server from "../environment";
 
-const server_url = `${server.baseURL}/socket.io`;
+const server_url = server;
 
 var connections = {};
 
