b18675ad92d510cbc9ebfc6716211425050397b4
priyankashori <shorishoripriyanka@gmail.com>
2026-07-03 12:10:51 +0530

    Update CORS configuration to allow specific origin

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,10 @@ const io = connectToSocket(server);
 dotenv.config();
 
 app.set("port", (process.env.PORT || 7000));
-app.use(cors());
+app.use(cors({
+    origin: "https://video-conferencing-frontend-6k2x.onrender.com",
+    credentials: true
+}));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
